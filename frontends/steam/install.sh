44fb7db97cb62be0afbebae604e64e711369dac4
Csaba Gaspar <cgaspar@accretivetg.com>
2024-10-26 11:30:04 +0200

    u

--- frontends/steam/install.sh
+++ frontends/steam/install.sh
@@ -8,12 +8,9 @@ source $CURRENT_ABSOLUTE_DIR/../../vars.env
 
 #controllers
 mkdir -p "$STEAM_CONTROLLERS_DIR/es-de"
-mkdir -p "$STEAM_CONTROLLERS_DIR/es-de/Icons"
-cp -r "$CURRENT_ABSOLUTE_DIR/configs/controllers/Icons/*.png" "$STEAM_CONTROLLERS_DIR/es-de/Icons"
-cp -r "$CURRENT_ABSOLUTE_DIR/configs/controllers/*.vdf" "$STEAM_CONTROLLERS_DIR/es-de"
+cp -r "$CURRENT_ABSOLUTE_DIR/configs/es-de" "$STEAM_CONTROLLERS_DIR"
 
 #shortcuts
 mkdir -p "$STEAM_CONFIG_DIR/grid"
-cp -r "$CURRENT_ABSOLUTE_DIR/configs/grid/*.png" "$STEAM_CONFIG_DIR/grid"
-cp -r "$CURRENT_ABSOLUTE_DIR/configs/grid/*.ico" "$STEAM_CONFIG_DIR/grid"
-cp "$CURRENT_ABSOLUTE_DIR/configs/shortcuts.vdf" "$STEAM_CONFIG_DIR/"
+cp -r "$CURRENT_ABSOLUTE_DIR/configs/grid" "$STEAM_CONFIG_DIR"
+cp "$CURRENT_ABSOLUTE_DIR/configs/shortcuts.vdf" "$STEAM_CONFIG_DIR"
