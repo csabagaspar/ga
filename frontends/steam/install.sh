ef95f7cd166078e7e88eedff645555b4cdd22a41
Csaba Gaspar <cgaspar@accretivetg.com>
2024-10-26 10:13:28 +0200

    update

--- frontends/steam/install.sh
+++ frontends/steam/install.sh
@@ -6,5 +6,10 @@ CURRENT_ABSOLUTE_DIR=$(
 )
 source $CURRENT_ABSOLUTE_DIR/../../vars.env
 
+#controllers
 cp $CURRENT_ABSOLUTE_DIR/configs/controllers/*.vdf $STEAM_CONTROLLERS_DIR/es-de
+
+#shortcuts
+mkdir -p $STEAM_CONFIG_DIR/grid
+cp $CURRENT_ABSOLUTE_DIR/configs/grid/*.* $STEAM_CONFIG_DIR/grid
 cp $CURRENT_ABSOLUTE_DIR/configs/shortcuts.vdf $STEAM_CONFIG_DIR
